d58548e36180ddab47c8a2ea6fffa57015379946
rhysemmas <rhys.emmas@neo4j.com>
2023-05-19 11:58:03 +0100

    Actually checkout to ref

--- main.go
+++ main.go
@@ -8,21 +8,27 @@ import (
 	"os/exec"
 
 	git "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/google/go-github/v52/github"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
 	token := os.Getenv("TOKEN")
 	if token == "" {
 		log.Fatal("TOKEN env var not set")
 	}
 
-	h := newHandler(token)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	workingDir := os.Getenv("WORKING_DIR")
+	if workingDir == "" {
+		workingDir = "/tmp/arduino-lora"
+	}
+
+	h := newHandler(token, workingDir)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", h.slashHandler)
@@ -37,11 +43,14 @@ func main() {
 }
 
 type handler struct {
-	token []byte
+	token      []byte
+	workingDir string
+
+	event *github.CreateEvent
 }
 
-func newHandler(token string) handler {
-	return handler{token: []byte(token)}
+func newHandler(token, workingDir string) handler {
+	return handler{token: []byte(token), workingDir: workingDir}
 }
 
 func (h *handler) slashHandler(w http.ResponseWriter, r *http.Request) {
@@ -62,35 +71,37 @@ func (h *handler) slashHandler(w http.ResponseWriter, r *http.Request) {
 
 	switch event := event.(type) {
 	case *github.CreateEvent:
-		h.handleGitHubCreateEvent(event)
+		h.event = event
+		h.handleGitHubCreateEvent()
 	}
 }
 
-func (h *handler) handleGitHubCreateEvent(event *github.CreateEvent) {
-	if *event.RefType == "tag" {
-		log.Printf("downloading version %v of arduino-lora", *event.Ref)
-		if err := gitCloneLatestCode(); err != nil {
-			log.Printf("error cloning latest code: %v", err)
-		}
+func (h *handler) handleGitHubCreateEvent() {
+	if *h.event.RefType != "tag" {
+		log.Printf("event not a tag, got ref: %v", *h.event.Ref)
+		return
+	}
 
-		log.Print("flashing arduino...")
-		if err := flashArduino(); err != nil {
-			log.Printf("error flashing arduino: %v", err)
-		} else {
-			log.Print("done!")
-		}
+	log.Printf("downloading version %v of arduino-lora", *h.event.Ref)
+	if err := h.gitCloneAndCheckoutRef(); err != nil {
+		log.Printf("error downloading code: %v", err)
+	}
 
-		log.Printf("cleaning up downloaded files")
-		if err := cleanupTmp(); err != nil {
-			log.Printf("error cleaning up /tmp: %v", err)
-		}
+	log.Print("flashing arduino...")
+	if err := h.flashArduino(); err != nil {
+		log.Printf("error flashing arduino: %v", err)
 	} else {
-		log.Printf("event not a tag, got ref: %v", *event.Ref)
+		log.Print("done!")
+	}
+
+	log.Printf("cleaning up downloaded files")
+	if err := h.cleanupDir(); err != nil {
+		log.Printf("error cleaning up %v: %v", h.workingDir, err)
 	}
 }
 
-func gitCloneLatestCode() error {
-	_, err := git.PlainClone("/tmp/arduino-lora", false, &git.CloneOptions{
+func (h *handler) gitCloneAndCheckoutRef() error {
+	repository, err := git.PlainClone(h.workingDir, false, &git.CloneOptions{
 		URL: "https://github.com/rhysemmas/arduino-lora",
 	})
 
@@ -98,12 +109,26 @@ func gitCloneLatestCode() error {
 		return fmt.Errorf("error cloning git repository: %v", err)
 	}
 
+	ref, err := repository.Reference(plumbing.ReferenceName(*h.event.Ref), true)
+	if err != nil {
+		return fmt.Errorf("error getting repository reference: %v", err)
+	}
+
+	workTree, err := repository.Worktree()
+	if err != nil {
+		return fmt.Errorf("error getting repository worktree: %v", err)
+	}
+
+	if err := workTree.Checkout(&git.CheckoutOptions{Hash: ref.Hash()}); err != nil {
+		return fmt.Errorf("error checking out to commit: %v", ref.Hash().String())
+	}
+
 	return nil
 }
 
-func flashArduino() error {
+func (h *handler) flashArduino() error {
 	cmd := exec.Command("pio", "run", "-t", "upload")
-	cmd.Dir = "/tmp/arduino-lora"
+	cmd.Dir = h.workingDir
 
 	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("error execing command: %v", err)
@@ -112,8 +137,8 @@ func flashArduino() error {
 	return nil
 }
 
-func cleanupTmp() error {
-	if err := os.RemoveAll("/tmp/arduino-lora"); err != nil {
+func (h *handler) cleanupDir() error {
+	if err := os.RemoveAll(h.workingDir); err != nil {
 		return fmt.Errorf("error calling os.Remove: %v", err)
 	}
 
